eec3874cc41bf54b6e96d9ef0ccc4253cf427be9
saik90305@gmail.com <saik90305@gmail.com>
2025-04-26 16:41:31 +0530

    added

--- mongodb.sh
+++ mongodb.sh
@@ -1,40 +1,53 @@
 #!/bin/bash
+
 DATE=$(date +%F)
 LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=$LOGDIR/$0-$DATE.log
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
-R="\e[31m"  # Red
-G="\e[32m"  # Green
-N="\e[0m"   # Reset
-Y="\e[33m"  # Yellow
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
-if [$USERID -ne 0];
-then 
-    echo -e "$R errror :: please run this script with root access $N"
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
 
 VALIDATE(){
-    if [ $1 -ne 0];
-    then 
-        echo -e "$2 ...$R FAILURE $N"
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
-    else 
-        echo -e " $2 .. $G success $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE  
-VALIDATE $? "coppied mongo.repo into yum .repos.d"
-yum install mongodb-org -y &>> $LOGFILE
-VALIDATE $? "installing mongodb"
-systemctl enable mongod &>> $LOGFILE
-VALIDATE $? "enabling mongodb"
-systemctl start mongod &>> $LOGFILE
-VALIDATE $? "starting mongodb"
-sed -i  's/127.0.0.1/0.0.0.0/' etc/mongod.conf &>> $LOGFILE
-VALIDATE $? "changing bind ip address"
-systemctl restart mongod 
-VALIDATE $? "restarting mongodb"
 
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copied MongoDB repo into yum.repos.d"
+
+yum install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installation of MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Edited MongoDB conf"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "Restarting MonogoDB"
